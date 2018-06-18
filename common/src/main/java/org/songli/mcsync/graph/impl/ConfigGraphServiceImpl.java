fd154b5b4f46548ad31343caec65ae778a1b0174
MrDefinite <lisongli199019@gmail.com>
2018-06-18 20:33:49 +0800

    Spring boot configs

---
+++ common/src/main/java/org/songli/mcsync/graph/impl/ConfigGraphServiceImpl.java
@@ -0,0 +1,15 @@
+package org.songli.mcsync.graph.impl;
+
+import org.songli.mcsync.config.Config;
+import org.songli.mcsync.graph.ConfigGraph;
+import org.songli.mcsync.graph.ConfigGraphService;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ConfigGraphServiceImpl implements ConfigGraphService {
+    public List<ConfigGraph> parseAndGetGraphs(Collection<Config> configs) {
+        throw new NotImplementedException();
+    }
+}
